2950914c359bb93d18a49b9d6b697aa4b20fbafa
brightwillies <bwboakye2@gmail.com>
2021-11-29 20:54:26 +0000

    update

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -15,14 +15,14 @@ class AdminSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'first_name' => 'Bright',
-            'last_name' => 'Williams',
-            'email' => 'admin@admin.com',
+            'first_name' => 'Joseline',
+            'last_name' => 'Mrs',
+            'email' => 'joseline_ballard@yahoo.com',
             'telephone_number' => '0241849088',
             'role_id' => 1,
             'status' => 1,
             'mask' => \generate_mask(),
-            'password' => Hash::make('Baking@2021!!..'),
+            'password' => Hash::make('NationwideShipping2021'),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
 
