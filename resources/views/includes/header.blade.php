8fdad89723988517c0a7afc2e2d0f8ea84355c62
brightwillies <bwboakye2@gmail.com>
2021-12-06 21:37:49 +0000

    update

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -38,7 +38,7 @@
 
                             <p  class="text-center">
                             Attention Shoppers: Pre-order for pickup locally from The Jelly Queens,
-                            <a target="_blank"  href="https://g.page/DFMDallasFarmersMarket_?share">
+                            <a target="_blank"  href="https://www.google.com/maps/dir//jelly+queens/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x8644d18e8ad497f5:0xc45322eb4ec2b458?sa=X&ved=2ahUKEwjBhd71j9D0AhXGyTgGHYmpBO8Q9Rd6BAhKEAU">
                             McKinney,TX  on December 23 & 24 2021.
                         </a>  </p>
                             <!-- <p  class="text-center">Attention Shoppers: Nationwide Shipping Only through GOLDBELLY Website! Preorder On Website Only If You Can Pickup Locally From the <a target="_blank"  style="color:#302020;" href="https://g.page/DFMDallasFarmersMarket_?share">Dallas Farmers Market</a>  , on Saturdays 10am - 3:30pm.</p> -->
